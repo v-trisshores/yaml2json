1479daebfffd859611c508047d80e743e3180412
v-trisshores <86677757+v-trisshores@users.noreply.github.com>
2023-06-12 13:43:25 -0700

    Initial commit

---
+++ code/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace yaml2json
+{
+    using System.IO;
+    using System.Text.Json;
+    using YamlDotNet.Serialization;
+
+    internal class Program
+    {
+        private static readonly string inputYamlFilePath = @"<YAML input file path>";
+        private static readonly string outputJsonFilePath = @"<JSON output file path>";
+
+        static void Main()
+        {
+            StreamReader sr = new(inputYamlFilePath);
+            Deserializer deserializer = new();
+            object? yamlObject = deserializer.Deserialize(sr);
+
+            Newtonsoft.Json.JsonSerializer jsonSerializer = new();
+            StringWriter sw = new();
+            jsonSerializer.Serialize(sw, yamlObject);
+            File.WriteAllText(outputJsonFilePath, PrettyJson(sw.ToString()));
+        }
+
+        public static string PrettyJson(string unPrettyJson)
+        {
+            JsonSerializerOptions options = new()
+            {
+                WriteIndented = true
+            };
+
+            JsonElement jsonElement = JsonSerializer.Deserialize<JsonElement>(unPrettyJson);
+
+            return JsonSerializer.Serialize(jsonElement, options);
+        }
+    }
+}
---
+++ docs/attribution.md
@@ -0,0 +1,4 @@
+# Attribution
+
+- YAML parsing using [YamlDotNet](https://github.com/aaubry/YamlDotNet)
+- JSON parsing using [Json.NET](https://www.newtonsoft.com/json)
